72131fc47f2145ef45623896bf4fd68d177bddb7
dracothedragon <vikram.gogte@gmail.com>
2019-10-14 22:02:39 -0700

    Update variables.tf

--- fortiadc/variables.tf
+++ fortiadc/variables.tf
@@ -45,7 +45,7 @@ variable "vm_image_ocid" {
 	// Example:
     //us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaaqxxs4ve4kweolwtw3afbhkqvvloyh77tmeb2a6i3mdmcykdqaidq"
 
-    us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaazgtma2esokdiqq7lwdekz3hy3wwggmtl7d7psgnzp46z2xxntrnq"
+    //us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaazgtma2esokdiqq7lwdekz3hy3wwggmtl7d7psgnzp46z2xxntrnq"
     //eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa7d3fsb6272srnftyi4dphdgfjf6gurxqhmv6ileds7ba3m2gltxq"
     //uk-london-1    = "ocid1.image.oc1.uk-london-1.aaaaaaaaa6h6gj6v4n56mqrbgnosskq63blyv2752g36zerymy63cfkojiiq"
   }
